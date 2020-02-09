b24511775f48e52abb502f3bb00b0aed2e1b2daf
Lucas Connell <lucas@unknown3ca9f40158c8.attlocal.net>
2020-02-09 09:04:13 -0600

    initial commit.

---
+++ app/templates/precache-manifest.9dd9e6b90afd497278c7e5d644f771da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bdbd6e24878adad1090",
+    "url": "../static/css/app.1968f6bd.css"
+  },
+  {
+    "revision": "8bdbd6e24878adad1090",
+    "url": "../static/js/app.3dcda19f.js"
+  },
+  {
+    "revision": "59fe323c34648444868d",
+    "url": "../static/js/chunk-2d219ff9.3b596ed2.js"
+  },
+  {
+    "revision": "4039b03a65729a89b1ec",
+    "url": "../static/css/chunk-vendors.745f5fd5.css"
+  },
+  {
+    "revision": "4039b03a65729a89b1ec",
+    "url": "../static/js/chunk-vendors.df3ae148.js"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "../static/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "../static/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "../static/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "../static/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "e7a0cf770ebc7f70aaec884929af831d",
+    "url": "index.html"
+  }
+];
